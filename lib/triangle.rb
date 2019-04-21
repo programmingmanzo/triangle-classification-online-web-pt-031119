3a86698dec8b52493635baa06efe9ca9efbb2095
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-21 04:43:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
       (@side_1 + @side_2 <= @side_3) || (@side_1 + @side_3 <= @side_2) || (@side_2 + @side_3 <= @side_1)
       raise TriangleError
     else 
-      if (@side_1 == @side_2 && @side_3) || ()
+      if (@side_1 == @side_2 && @side_3) || (@side_2 == @side_1 && @side_2) || ()
         :equilateral
         elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
         :scalene 
